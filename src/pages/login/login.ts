f86af8fb6e2b66435a88f2699172f8a38fcc0541
Manish Arora <manish.mastishka@gmail.com>
2018-05-04 15:45:14 +0530

    intermittent check-in

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import {TabsPage} from '../tabs/tabs';
+import { TabsPage } from '../tabs/tabs';
 
 import { SingletonServiceProvider } from '../../providers/singleton-service/singleton-service';
 
